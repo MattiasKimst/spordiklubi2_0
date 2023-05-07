26f44ec547e108c8bb5f7a9da8d51797c4493157
MattiasKimst <mattias.kimst@mail.ee>
2023-05-07 19:31:54 +0300

    veateade logimisel, kui vale isikukood; käitumine akna suuruse muutmisel; veel pisivigu

--- src/main/java/com/example/spordiklubi2_0/Rakendus.java
+++ src/main/java/com/example/spordiklubi2_0/Rakendus.java
@@ -3,14 +3,18 @@ package com.example.spordiklubi2_0;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.beans.binding.NumberBinding;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -25,12 +29,18 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.example.spordiklubi2_0.Peaklass.*;
+
 import static java.lang.Integer.parseInt;
+import static javafx.scene.paint.Color.RED;
 
 public class Rakendus extends Application {
 
+
+    // meetod, mis teeb kindlaks, kas etteantud isikukoodiga isik on spordiklubi liige
     public boolean tuvastaInimene(String isikukood, List<Liige> isikud) {
         for (Liige inimene : isikud
         ) {
@@ -42,6 +52,8 @@ public class Rakendus extends Application {
         return false;
     }
 
+
+    //meetod, mis leiab listina etteantud isikukoodi järgi selle isiku broneeirngud
     public List<Broneering> leiaIsikuBronnid(String isikukood, List<Broneering> broneeringud, List<Liige> isikud) {
         List<Broneering> bronnid = new ArrayList<>();
         for (Liige inimene : isikud
@@ -60,6 +72,8 @@ public class Rakendus extends Application {
         return bronnid;
     }
 
+
+    //meetod, mis leiab isikukoodi järgi Liige tüüpi objekti
     public Liige leiaisik(String isikukood, List<Liige> isikud) {
         for (Liige liige : isikud
         ) {
@@ -73,57 +87,24 @@ public class Rakendus extends Application {
     @Override
     public void start(Stage pealava) throws IOException {
 
-        //andmed
+        //nendes listides hoiame andmeid
         List<Liige> isikud = loeSisseIsikud("isikud.txt");
         List<Treening> trennid = loeSissetreeningud("treeningud.txt");
         List<Broneering> broneeringud = loeSisseBroneeringud("broneeringud.txt", trennid, isikud);
 
 
-        //akna suurused
-        int aknaLaius = 600;
-        int aknaKõrgus = 600;
+
+        //akna suuruse algväärtused
+       int aknaLaius=600;
+       int aknaKõrgus=600;
 
 
-        //nupusuurused
-
+        //nupuvahede suurused avalehel
         int nupuVahe = 20;
         int laius = 270;
 
 
 
-        //avalehe stseen
-
-        //avalehe nupud
-        Button kuvaBronnid = new Button("Kuva broneeringud");
-        Button kuvaTrennid = new Button("Kuva treeningud");
-        Button soovitaTrenni = new Button("Soovita treeningut");
-        Button kuvaTulevased = new Button("Kuva enda tulevased treeningud");
-
-
-        GridPane ruudustik = new GridPane();
-
-        ruudustik.add(kuvaBronnid, 0, 3);
-        ruudustik.add(kuvaTrennid, 0, 4);
-        ruudustik.add(soovitaTrenni, 0, 5);
-        ruudustik.add(kuvaTulevased, 0, 6);
-        Text menüüPealkiri = new Text("Spordiklubi 2.0");
-        menüüPealkiri.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
-        ruudustik.add(menüüPealkiri, 0, 0, 2, 1);
-        Text menüü = new Text("Tee oma valik: ");
-        menüü.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        ruudustik.add(menüü, 0, 2, 2, 1);
-        Text vahe = new Text(" ");
-        ruudustik.add(vahe, 0, 1, 2, 1);
-
-
-        ruudustik.setVgap(nupuVahe);
-        ruudustik.setAlignment(Pos.CENTER);
-        ruudustik.setMinWidth(laius);
-        ruudustik.setMaxWidth(laius);
-
-        Scene avaleheStseen = new Scene(ruudustik, aknaLaius, aknaKõrgus);
-
-        //avalehe lõpp
 
 
 
@@ -153,35 +134,90 @@ public class Rakendus extends Application {
         Label parool = new Label("Parool");
         grid.add(parool, 0, 8);
 
-        PasswordField parooliVäli = new PasswordField();
+        PasswordField parooliVäli = new PasswordField(); //parooliväli on olemas lihtsalt toreduse pärast, et asi meenutaks päris programmi; parooli kusagil ei kasutata
         grid.add(parooliVäli, 1, 8);
 
-        Label vihje = new Label("Vihje: kasuta 39912126758, parooli pole vaja");
+        Label vihje = new Label("Vihje: kasuta 39912126758, parooli pole vaja"); //info, kuidas programmi testida
         grid.add(vihje, 1, 12);
 
 
         Button logimisNupp = new Button("Logi sisse");
-        Button tagasiNuppLogimine = new Button("Tagasi");
 
         HBox logimisHBox = new HBox(10);
 
         logimisHBox.setAlignment(Pos.BOTTOM_RIGHT);
         logimisHBox.getChildren().add(logimisNupp);
-        logimisHBox.getChildren().add(tagasiNuppLogimine);
+
         grid.add(logimisHBox, 1, 10);
 
-        Scene logimisStseen = new Scene(grid, aknaLaius, aknaKõrgus);
+        Label hoiatus= new Label(""); //kasutame seda, kui isikukood on vale
+        hoiatus.setTextFill(RED);
+        grid.add(hoiatus, 1, 13);
 
-        final String[] kasutajaIsikukood = new String[1];
+        Scene logimisStseen = new Scene(grid, aknaLaius,aknaKõrgus);
+
+
+        final String[] kasutajaIsikukood = new String[1];//java ei lubanud lambda avaldistes lihtsalt String tüüpi muutujat kasutada, seetõttu on String[] tüüpi
         final String[] kasutajaParool = new String[1];
 
 
-        logimisNupp.setOnMousePressed(sündmus -> {
-            kasutajaIsikukood[0] = isikukoodiVäli.getText();//kasutajaisikukoodis hoiame läbivalt isikukoodi, mille saime logimisel, String[] tüüpi pikkusega üks, sest lambda avaldistes tavaline String ei saa olla
-            kasutajaParool[0] = parooliVäli.getText();//salvestame siia lihtsalt paroolivälja info, midagi sellega peale ei hakka
-            pealava.setScene(avaleheStseen);
+         //logimise lõpp
 
-        }); //logimise lõpp
+
+
+
+
+
+
+
+
+        //avalehe stseen
+
+        //avalehe nupud
+        Button kuvaBronnid = new Button("Kuva broneeringud");
+        Button kuvaTrennid = new Button("Kuva treeningud");
+        Button soovitaTrenni = new Button("Soovita treeningut");
+        Button kuvaTulevased = new Button("Kuva enda tulevased treeningud");
+
+
+        GridPane ruudustik = new GridPane();
+
+        ruudustik.add(kuvaBronnid, 0, 3);
+        ruudustik.add(kuvaTrennid, 0, 4);
+        ruudustik.add(soovitaTrenni, 0, 5);
+        ruudustik.add(kuvaTulevased, 0, 6);
+
+        Text menüüPealkiri = new Text("Spordiklubi 2.0");
+        menüüPealkiri.setFont(Font.font("Tahoma", FontWeight.NORMAL, 40));
+        ruudustik.add(menüüPealkiri, 0, 0, 2, 1);
+
+        Text menüü = new Text("Tee oma valik: ");
+        menüü.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        ruudustik.add(menüü, 0, 2, 2, 1);
+
+        Text vahe = new Text(" ");
+        ruudustik.add(vahe, 0, 1, 2, 1);
+
+
+        ruudustik.setVgap(nupuVahe);
+        ruudustik.setAlignment(Pos.CENTER);
+        ruudustik.setMinWidth(laius);
+        ruudustik.setMaxWidth(laius);
+
+        Scene avaleheStseen = new Scene(ruudustik,pealava.getWidth(), pealava.getHeight());
+
+
+
+        //avalehe lõpp
+
+
+
+
+
+
+
+
+
 
 
         // treeningute kuvamise steen, tabeli loomine lehelt https://jenkov.com/tutorials/javafx/tableview.html
@@ -235,15 +271,16 @@ public class Rakendus extends Application {
 
                 Treening valitudTrenn = (Treening) tableView.getSelectionModel().getSelectedItem();
                 Liige broneerija = leiaisik(kasutajaIsikukood[0], isikud);
-                Label teade = new Label(Broneering.teeBroneering(valitudTrenn.getNimi(), broneerija, trennid, broneeringud));
+                String teade = Broneering.teeBroneering(valitudTrenn.getNimi(), broneerija, trennid, broneeringud);
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);//kuvab dialoogiakna slavestamise õnnestumise kohta
+                alert.setTitle("Broneerimine");
+                alert.setHeaderText(null);
+                alert.setContentText(teade);
+
+                alert.showAndWait();
 
 
-                VBox vbox4 = new VBox(teade); //see koodijupp kordub palju, vaja kuidagi ühe korraga lahendada
-                Button tagasinuppTeateStseen = new Button("Tagasi");
-                vbox4.getChildren().add(tagasinuppTeateStseen);
-                Scene TeateStseen = new Scene(vbox4, 500, 500);
-                pealava.setScene(TeateStseen);
-                tagasinuppTeateStseen.setOnMousePressed(sündmus -> pealava.setScene(avaleheStseen));
             }
 
 
@@ -257,9 +294,18 @@ public class Rakendus extends Application {
         kuidasBroneerida.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
         vboxTrennideKuvamine.getChildren().add(kuidasBroneerida);
         vboxTrennideKuvamine.getChildren().add(tagasiNuppTrennid);
-        Scene trennideStseen = new Scene(vboxTrennideKuvamine, aknaLaius, aknaKõrgus);
+
+        Scene trennideStseen = new Scene(vboxTrennideKuvamine, pealava.getWidth(), pealava.getHeight());
+
 
         //treeningute kuvamise lõpp
+
+
+
+
+
+
+
 
 
         //Broneeringute kuvamise stseen
@@ -298,12 +344,21 @@ public class Rakendus extends Application {
         Button tagasiNuppBronnid = new Button("Tagasi");
         vboxBroneeringuteKuvamine.getChildren().add(tagasiNuppBronnid);
 
-        Scene stseenBroneeringud = new Scene(vboxBroneeringuteKuvamine, aknaLaius, aknaKõrgus);
+        Scene stseenBroneeringud = new Scene(vboxBroneeringuteKuvamine, pealava.getWidth(), pealava.getHeight());
+
+
 
         //broneeringute kuvamise lõpp
 
 
-        // soovituse ekraan
+
+
+
+
+
+
+
+        // soovituse stseen
 
 
         List<Broneering> bronnid = leiaIsikuBronnid(kasutajaIsikukood[0], broneeringud, isikud);
@@ -313,17 +368,31 @@ public class Rakendus extends Application {
         Label soovitus = new Label(Treening.soovitaTreeningut(inimene, bronnid, trennid));
         soovitus.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 
+        GridPane ruudustikSoovitus = new GridPane();
+        ruudustikSoovitus.add(soovitus, 0, 1);
 
-        VBox vboxSoovitaTrenni = new VBox(soovitus);
+
+        ruudustikSoovitus.setVgap(nupuVahe);
+        ruudustikSoovitus.setAlignment(Pos.CENTER);
+
         Button tagasiNuppSoovitus = new Button("Tagasi");
-        vboxSoovitaTrenni.getChildren().add(tagasiNuppSoovitus);
-        Scene trenniSoovituseStseen = new Scene(vboxSoovitaTrenni, aknaLaius, aknaKõrgus);
+        ruudustikSoovitus.add(tagasiNuppSoovitus, 0, 2);
 
 
-        //soovituse kuvamise lõpp
 
 
-        //Enda tulevaste broneeringute vaatamine
+
+        Scene trenniSoovituseStseen = new Scene(ruudustikSoovitus,pealava.getWidth(),pealava.getHeight());
+
+        //soovituse stseeni lõpp
+
+
+
+
+
+
+
+        //Enda tulevaste broneeringute vaatamise stseen
 
         kuvaTulevased.setOnMousePressed(sündmus ->
 
@@ -366,32 +435,133 @@ public class Rakendus extends Application {
         VBox vboxKuvaTulevased = new VBox(tableView3);
             Button tagasiNuppKuvaTulevased = new Button("Tagasi");
             vboxKuvaTulevased.getChildren().add(tagasiNuppKuvaTulevased);
-            Scene stseenEndaTreeningud = new Scene(vboxKuvaTulevased, aknaLaius, aknaKõrgus);
+            Scene stseenEndaTreeningud = new Scene(vboxKuvaTulevased, pealava.getWidth(),pealava.getHeight());
+
+            final int lavaLaius = (int) pealava.getWidth();
+            final int lavaKõrgus =(int) pealava.getHeight();
+
             pealava.setScene(stseenEndaTreeningud);
-            tagasiNuppKuvaTulevased.setOnMousePressed(sündmus2 -> pealava.setScene(avaleheStseen));
+            pealava.setWidth(lavaLaius);
+            pealava.setHeight(lavaKõrgus);
+
+
+            tagasiNuppKuvaTulevased.setOnMousePressed(sündmus2 -> {
+
+                pealava.setScene(avaleheStseen); });
 
         });
-        //enda tulevaste vaatamise lõpp
+        //enda tulevaste vaatamise stseeni lõpp
+
+
+
+
+
+
+
+
+        //logimisnupu vajutamise tegevused:
+
+        logimisNupp.setOnMousePressed(sündmus -> {
+
+            if (tuvastaInimene(isikukoodiVäli.getText(),isikud)){//meetod tuvastainimene tagastab true, kui vastav isikukood kuulub mõnele liikmele
+                kasutajaIsikukood[0] = isikukoodiVäli.getText();//kasutajaisikukoodis hoiame läbivalt isikukoodi, mille saime logimisel, String[] tüüpi pikkusega üks, sest lambda avaldistes tavaline String ei saa olla
+                kasutajaParool[0] = parooliVäli.getText();//salvestame siia lihtsalt paroolivälja info, midagi sellega peale ei hakka
+
+
+                //jätame enne stseeni vahetamist meelde eelneva aknasuuruse ja peale stseeni vahetamist kanname selle uuele stseenile üle,
+                //muidu kui muuta mingi stseeni jookul akna suurust, siis stseenivahetusel taastub algväärtus, paremat võimalust selle lahendamiseks ei leidnud
+                final int lavaLaius = (int) pealava.getWidth();
+                final int lavaKõrgus =(int) pealava.getHeight();
+                pealava.setScene(avaleheStseen);
+                pealava.setWidth(lavaLaius);
+                pealava.setHeight(lavaKõrgus);
+
+
+            }
+            else {
+                hoiatus.setText("Vigane isikukood või ei ole sellise isikukoodiga liiget");//kui liikmete seas pole vastavat isikukoodi, viskame erindi
+
+            }
+        });
+
+
 
 
         //avaekraani nuppude tegevused
 
 
-        kuvaTrennid.setOnMousePressed(sündmus -> pealava.setScene(trennideStseen));
-        kuvaBronnid.setOnMousePressed(sündmus -> pealava.setScene(stseenBroneeringud));
-        soovitaTrenni.setOnMousePressed(sündmus -> pealava.setScene(trenniSoovituseStseen));
+        kuvaTrennid.setOnMousePressed(sündmus ->{
+            //jätame enne stseeni vahetamist meelde eelneva aknasuuruse ja peale stseeni vahetamist kanname selle uuele stseenile üle,
+            //muidu kui muuta mingi stseeni jookul akna suurust, siis stseenivahetusel taastub algväärtus, paremat võimalust selle lahendamiseks ei leidnud
+            final int lavaLaius = (int) pealava.getWidth();
+            final int lavaKõrgus =(int) pealava.getHeight();
+            pealava.setScene(trennideStseen);
+            pealava.setWidth(lavaLaius);
+            pealava.setHeight(lavaKõrgus);
+        });
 
 
-        tagasiNuppLogimine.setOnMousePressed(sündmus -> pealava.setScene(avaleheStseen));
-        tagasiNuppTrennid.setOnMousePressed(sündmus -> pealava.setScene(avaleheStseen));
-        tagasiNuppBronnid.setOnMousePressed(sündmus -> pealava.setScene(avaleheStseen));
-        tagasiNuppSoovitus.setOnMousePressed(sündmus -> pealava.setScene(avaleheStseen));
+
+        kuvaBronnid.setOnMousePressed(sündmus -> {
+            final int lavaLaius = (int) pealava.getWidth();
+            final int lavaKõrgus =(int) pealava.getHeight();
+
+            pealava.setScene(stseenBroneeringud);
+            pealava.setWidth(lavaLaius);
+            pealava.setHeight(lavaKõrgus);
+
+        });
+
+
+        soovitaTrenni.setOnMousePressed(sündmus -> {
+
+            final int lavaLaius = (int) pealava.getWidth();
+            final int lavaKõrgus =(int) pealava.getHeight();
+
+            pealava.setScene(trenniSoovituseStseen);
+            pealava.setWidth(lavaLaius);
+            pealava.setHeight(lavaKõrgus);
+        });
+
+
+
+        tagasiNuppTrennid.setOnMousePressed(sündmus -> {
+
+            final int lavaLaius = (int) pealava.getWidth();
+            final int lavaKõrgus =(int) pealava.getHeight();
+
+            pealava.setScene(avaleheStseen);
+            pealava.setWidth(lavaLaius);
+            pealava.setHeight(lavaKõrgus);});
+
+
+        tagasiNuppBronnid.setOnMousePressed(sündmus -> {
+
+            final int lavaLaius = (int) pealava.getWidth();
+            final int lavaKõrgus =(int) pealava.getHeight();
+
+            pealava.setScene(avaleheStseen);
+            pealava.setWidth(lavaLaius);
+            pealava.setHeight(lavaKõrgus); });
+
+
+
+        tagasiNuppSoovitus.setOnMousePressed(sündmus -> {
+            final int lavaLaius = (int) pealava.getWidth();
+            final int lavaKõrgus =(int) pealava.getHeight();
+
+            pealava.setScene(avaleheStseen);
+            pealava.setWidth(lavaLaius);
+            pealava.setHeight(lavaKõrgus); });
+
 
 
         pealava.setScene(logimisStseen);
         pealava.setTitle("Spordiklubi");
 
         pealava.show();
+
+
 
 
        pealava.setOnCloseRequest(new EventHandler<WindowEvent>() {
@@ -401,9 +571,22 @@ public class Rakendus extends Application {
 
                 try {
                     kirjutaBroneeringudFaili("broneeringud.txt", broneeringud);
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);//kuvab dialoogiakna slavestamise õnnestumise kohta
+                    alert.setTitle("Salvestamine");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Broneeringud edukalt salvestatud ");
+
+                    alert.showAndWait();
+                    //throw new IOException(); //kui salvestamine ebaõnnestub, tuleb ette aken, mis ütleb seda; see rida on  tolle akna testimiseks
                 } catch (IOException e) {
-                    System.out.println("Broneeringute faili kirjutamine ebaõnnestus. Põhjus: " +e.getMessage());
-                    throw new RuntimeException(e);
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Salvestamine ebaõnnestus");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Broneeringute faili kirjutamine ebaõnnestus. ");
+
+                    alert.showAndWait();
+
+
                 }
                 Platform.exit();
                 System.exit(0);
